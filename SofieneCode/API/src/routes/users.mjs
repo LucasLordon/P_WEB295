fe47aee1d5c4b5301ea0f2c63b7da313e936c951
termimi <123395118+termimi@users.noreply.github.com>
2024-02-29 19:51:07 +0100

    DB request ok

--- SofieneCode/API/src/routes/users.mjs
+++ SofieneCode/API/src/routes/users.mjs
@@ -1,46 +1,48 @@
 import express from "express";
 import { dataUsers, getUser,removeUser,updateUser } from "../db/mock-user.mjs";
 import { success,getUniqueUserId } from "./helper.mjs";
+import { UserTable } from "../db/sequelize.mjs";
 
 const usersRouter = express();
 
 usersRouter.get("/",(req,res) => {
-    const message = "La liste des produits a bien été récupérée.";
-    res.json(success(message, dataUsers));
+    UserTable.findAll().then((usersData) => {
+        const message = "La liste des utilisateurs a bien été récupérée.";
+        res.json(success(message, usersData));
+    })  
 });
 usersRouter.get("/:id",(req,res) => {
-    let userID = req.params.id;
-    let dataUserFind = dataUsers.find((user) => user.id == userID);
-    const message = `Le produit dont l'id vaut ${dataUserFind} a bien été récupéré.`;
-    res.json(success(message,dataUserFind))
+    UserTable.findByPk(req.params.id).then((usersData) => {
+        const message = `L'utilisateur dont l'id vaut ${usersData.id} a bien été récupéré.`;
+        res.json(success(message,usersData))
+    });
 });
+//Insomnia
 usersRouter.post("/",(req,res) => {
-    const id = getUniqueUserId(dataUsers)
-    let createdUser = {...req.body, ...{id: id, Date: new Date()}};
-    dataUsers.push(createdUser);
-    const message = `L'utilisateur ${createdUser.pseudo} à bien été créé`;
-    res.json(success(message,createdUser));
+    UserTable.create(req.body).then((userCreated) => {
+        const message = `L'utilisateur ${userCreated.pseudo} à bien été créé`;
+        res.json(success(message,userCreated));
+    })
 });
-
+// insomnia
 usersRouter.delete("/:id",(req,res) => {
-    let userID = req.params.id
-    let deletedUser = getUser(userID);
-    removeUser(userID);
-    const message = `L'utilisateur ${deletedUser.pseudo} à bien été supprimé`;
-    res.json(success(message,deletedUser));
+    UserTable.findByPk(req.params.id).then((userDeleted) => {
+        UserTable.destroy({
+            where: {id: userDeleted.id },
+        })
+    }).then((_) => {
+        const message = `L'utilisateur ${userDeleted.pseudo} à bien été supprimé`;
+        res.json(success(message,userDeleted));
+    });
 });
-
+// insomnia
 usersRouter.put("/:id",(req,res) => {
     let userID = req.params.id;
-    let user = getUser(userID);
-
-    const updatedUser = {
-        id: userID,
-        ...req.body,
-        created: user.created
-    };
-    updateUser(userID, updatedUser);
-    const message = `Le produit ${user.pseudo} dont l'id vaut ${userID} a été mis à jour avec succès !`;
-    res.json(success(message,updatedUser));
+    UserTable.update(req.body, {where :{id: userID}}).then((_) => {
+        UserTable.findByPk(userID).then((userFinded) => {
+            const message = `L'utilisateur ${userFinded.pseudo} dont l'id vaut ${userID} a été mis à jour avec succès !`;
+            res.json(success(message,userFinded));
+        })
+    });
 });
 export {usersRouter};
