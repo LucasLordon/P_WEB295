61c546dbc917f58713e880fcd3c43989568e7f09
termimi <123395118+termimi@users.noreply.github.com>
2024-03-02 08:45:27 +0100

    ajout gestion erreur sur route put et del

--- SofieneCode/API/src/routes/users.mjs
+++ SofieneCode/API/src/routes/users.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { dataUsers, getUser,removeUser,updateUser } from "../db/mock-user.mjs";
 import { success,getUniqueUserId } from "./helper.mjs";
 import { UserTable } from "../db/sequelize.mjs";
+import { Error } from "sequelize";
 
 const usersRouter = express();
 
@@ -17,32 +18,40 @@ usersRouter.get("/:id",(req,res) => {
         res.json(success(message,usersData))
     });
 });
-//Insomnia
+
 usersRouter.post("/",(req,res) => {
     UserTable.create(req.body).then((userCreated) => {
         const message = `L'utilisateur ${userCreated.pseudo} à bien été créé`;
         res.json(success(message,userCreated));
     })
 });
-// insomnia
+
 usersRouter.delete("/:id",(req,res) => {
+    let deletedUser;
     UserTable.findByPk(req.params.id).then((userDeleted) => {
+        deletedUser = userDeleted;
         UserTable.destroy({
             where: {id: userDeleted.id },
         })
     }).then((_) => {
-        const message = `L'utilisateur ${userDeleted.pseudo} à bien été supprimé`;
-        res.json(success(message,userDeleted));
+        const message = `L'utilisateur ${deletedUser.pseudo} à bien été supprimé`;
+        res.json(success(message,deletedUser));
+    }).catch((Error) => {
+        const message = `L'utilisateur ${req.params.id} n'as pas été trouver`;
+        res.json(success(message,Error));
     });
 });
-// insomnia
+
 usersRouter.put("/:id",(req,res) => {
     let userID = req.params.id;
     UserTable.update(req.body, {where :{id: userID}}).then((_) => {
         UserTable.findByPk(userID).then((userFinded) => {
             const message = `L'utilisateur ${userFinded.pseudo} dont l'id vaut ${userID} a été mis à jour avec succès !`;
             res.json(success(message,userFinded));
-        })
+        }).catch((Error) => {
+            const message = `L'utilisateur ayant l'id ${userID} n'as pas pu être modifier`;
+            res.json(success(message,Error));
+        });
     });
 });
 export {usersRouter};
