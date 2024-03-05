d759f67c85e555ac1f9238956996db6a017819c9
termimi <123395118+termimi@users.noreply.github.com>
2024-03-05 10:08:49 +0100

    fusion code Lucas et Sofiène

---
+++ code/API/src/app.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import { usersRouter } from "./routes/users.mjs";
+import { initDB, sequelize } from "./db/sequelize.mjs";
+import { loginRouter } from "./routes/login.mjs";
+
+
+const app = express();
+const port = 3000;
+ sequelize
+    .authenticate()
+    .then((_) => console.log("la connexion à la base de donnée à bien été établie")
+    )
+    .catch((error) => console.error("Impossible de se connecter à la db"));
+initDB();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+app.get("/api/", (req, res) => {
+  res.redirect(`http://localhost:${port}`);
+});
+app.use("/api/users", usersRouter);
+app.use("/api/login",loginRouter);
+
+app.use(({res}) => {
+  const message = "Impossible de trouver la ressource demandée ! Veuillez essayer une autre URL";
+  res.status(404).json(message);
+});
+
---
+++ code/API/src/db/sequelize.mjs
@@ -0,0 +1,82 @@
+import { DataTypes, Sequelize } from "sequelize";
+import { dataUsers } from "./mock-user.mjs";
+import { UserModel } from "../model/userModel.mjs";
+import { BookModel } from "../model/t_books.mjs";
+import { CategoryModel } from "../model/t_categorys.mjs";
+import { dataBooks } from "../db/mock-book.mjs";
+import { dataCategory } from "../db/mock-category.mjs";
+import bcrypt from 'bcrypt';
+
+const sequelize = new Sequelize(
+    "db_librairie",
+    "root",
+    "root",
+    {
+        host: "localhost",
+        port: 6033,
+        dialect : "mysql",
+        logging: false,
+    }
+);
+
+
+const UserTable = UserModel(sequelize,DataTypes);
+const modelBook = BookModel(sequelize, DataTypes);
+const modelCategory = CategoryModel(sequelize, DataTypes);
+
+modelCategory.hasMany(modelBook,{
+    foreignKey: "categories_id",
+})
+
+modelBook.belongsTo(modelCategory, {
+    foreignKey: "categories_id",
+})
+
+let initDB = () =>{
+    return sequelize
+    .sync({force:true})
+    .then((_) => {
+        importUser();
+        importCategory();
+        importBooks();
+        console.log("la base de donnée db_librairie a bien été créé");
+    });
+};
+
+const importUser = () => {
+    dataUsers.map((user) => {
+        bcrypt
+        .hash(user.mot_de_passe,10)
+        .then((hash) => {
+            UserTable.create({
+                pseudo : user.pseudo,
+                date_entree: user.date_entree,
+                mot_de_passe: hash
+            }).then((user) => console.log(user.toJSON()));
+        })
+    });
+};
+
+const importBooks = () => {
+    // import tous les produits présents dans le fichier db/mock-product
+    dataBooks.map((book) => {
+        modelBook.create({
+            price: book.price,
+            title: book.title,
+            image: book.image,
+            categories_id: book.categories_id,
+            page_count: book.page_count,
+            summary: book.summary
+        }).then((book) => console.log(book.toJSON()));
+    });
+};
+
+const importCategory = () => {
+    // import tous les produits présents dans le fichier db/mock-product
+    dataCategory.map((category) => {
+        modelCategory.create({
+            category: category.category,
+        }).then((category) => console.log(category.toJSON()));
+    });
+};
+export {sequelize,initDB,UserTable};
