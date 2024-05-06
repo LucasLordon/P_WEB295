c682d7238b6ddc38e564cb1b869c2d5e8fd1aecd
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-05-06 10:20:00 +0200

    add Api, edit Vue homeView.vue

--- FrontEnd/Lucas/WIP/Code/P-library/src/views/HomeView.vue
+++ FrontEnd/Lucas/WIP/Code/P-library/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 
+import Book from '@/components/randomBook/book.vue'
+import Author from '@/components/randomBook/author.vue';
+import Comments from '@/components/randomBook/comments.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="randomBook">
+    <Book></Book>
+    <Author></Author>
+    <Comments></Comments>
+  </div>
 </template>
