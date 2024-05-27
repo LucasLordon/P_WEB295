64464ae6d899641abbbc61dce1e38ec6f6ae89e5
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-05-27 11:22:18 +0200

    firebase !!!!

--- FrontEnd/Lucas/WIP/Code/P-library/src/views/AboutView.vue
+++ FrontEnd/Lucas/WIP/Code/P-library/src/views/AboutView.vue
@@ -1,8 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <input type="file" @change="handlerFileUpload">
+    <button @click="uploadFile">Upload</button>
+
+    <div v-for="(image, index) in imageUrls" :key="index">
+      <img :src="image" alt="Image">
+    </div>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+import storage from './firebase.js';
+
+export default {
+  setup() {
+    const selectedFile = ref(null);
+    const imageUrls = ref([]);
+
+    const handlerFileUpload = (event) => {
+      const file = event.target.files[0];
+      selectedFile.value = file;
+    };
+
+    const uploadFile = async () => {
+      if (selectedFile.value) {
+        const storageRef = storage.ref();
+        const fileRef = storageRef.child(selectedFile.value.name);
+        await fileRef.put(selectedFile.value);
+        console.log('File uploaded successfully!');
+        // Rafraîchir la liste des images après le téléversement
+        await refreshImageUrls();
+      }
+    };
+
+    const refreshImageUrls = async () => {
+      // Référence au stockage Firebase
+      const storageRef = storage.ref();
+
+      try {
+        // Récupérer la liste des fichiers dans le stockage Firebase
+        const listResult = await storageRef.listAll();
+
+        // Réinitialiser la liste des URL des images
+        imageUrls.value = [];
+
+        // Pour chaque fichier trouvé, obtenir l'URL de téléchargement
+        await Promise.all(listResult.items.map(async (item) => {
+          const imageUrl = await item.getDownloadURL();
+          imageUrls.value.push(imageUrl);
+        }));
+      } catch (error) {
+        console.error('Erreur lors de la récupération des images :', error);
+      }
+    };
+
+    // Appel initial pour afficher les images existantes
+    refreshImageUrls();
+
+    return {
+      handlerFileUpload,
+      uploadFile,
+      imageUrls,
+    };
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
